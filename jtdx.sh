9b0775d59d06dc5d0fba9f1a936ca5c6a97631e5
W7SVT <W7SVT@icloud.com>
2021-09-02 10:02:02 -0700

    Switched from armhf to ARM64 Now building from source

--- jtdx.sh
+++ jtdx.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
 #########################################################
-# Created by W7SVT Nov 2021 #############################
+# Created by W7SVT Nov 2020 #############################
+# Created by W7SVT Sep 2021 #############################
 #########################################################
 #########################################################
 #  __      ___________  _____________   _______________ #
@@ -11,31 +12,75 @@
 #   \__/\  /   /____/ /_______  /   \___/     |____|    #
 #        \/                   \/                        #
 #########################################################
-cd $HOME/Downloads
+cd $HOME/
+sudo apt-get install -y \
+asciidoctor \
+libfftw3-dev \
+qtdeclarative5-dev \
+texinfo \
+libqt5multimedia5 \
+libqt5multimedia5-plugins \
+qtmultimedia5-dev \
+libusb-1.0.0-dev \
+libqt5serialport5-dev \
+asciidoc \
+libudev-dev \
+libboost-all-dev \
+libwebsockets-dev \
+
 
 echo "######################" 
-echo "# Downloading JTDX #"
-echo "######################" 
-# TODO: Scrape is broken
-# jtdx_dl=$(curl -sL https://www.jtdx.tech/downloads/ | \
-#     tac | \
-#     grep armhf.deb | \
-#     grep -v rc | \
-#     awk -F'"' '$0=$2'
-# )
-cd $HOME/Downloads
-
-
-if [ ! -f $HOME/Downloads/jtdx-2.2.0-rc155_r_armhf.deb ]; then
-    wget -t 5 https://www.jtdx.tech/downloads/Linux/jtdx-2.2.0-rc155_r_armhf.deb
-
-fi
-
-echo "######################" 
-echo "# Installing JTDX  #"
+echo "# Setting Temp Swap #"
 echo "######################" 
 
-sudo apt-get install $HOME/Downloads/jtdx-2.2.0-rc155_r_armhf.deb -y
+sudo fallocate -l 2G /swapfile 
+sudo chmod 600 /swapfile 
+sudo mkswap /swapfile 
+sudo swapon /swapfile
+
+cd $HOME
+
+echo "###########################" 
+echo "# Downloading JTDX HAMLIB #"
+echo "###########################" 
+
+mkdir ~/hamlib-prefix
+cd ~/hamlib-prefix
+git clone git://github.com/jtdx-project/jtdxhamlib src
+
+echo "###########################" 
+echo "# Build JTDX HAMLIB from src #"
+echo "###########################" 
+
+cd src
+./bootstrap
+mkdir ../build
+cd ../build
+../src/configure --prefix=$HOME/hamlib-prefix \
+ --disable-shared --enable-static --without-readline \
+ --without-indi --without-cxx-binding --disable-winradio \
+ CFLAGS="-g -O2 -fdata-sections -ffunction-sections" \
+ LDFLAGS="-Wl,--gc-sections"
+make
+make install-strip
+cd $HOME
+
+echo "######################" 
+echo "# Download JTDX         #"
+echo "######################" 
+
+mkdir -p ~/jtdx-prefix/build
+cd ~/jtdx-prefix
+git clone git://github.com/jtdx-project/jtdx src
+
+echo "###########################" 
+echo "# Build JTDX              #"
+echo "###########################" 
+
+cd ~/jtdx-prefix/build
+cmake -D CMAKE_PREFIX_PATH=~/hamlib-prefix ../src
+cmake --build .
+sudo cmake --build . --target install
 
 echo "######################" 
 echo "# default config JTDX  #"
