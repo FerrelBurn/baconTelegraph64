198c76eadfbff1a614e7897794c3923f79394c57
W7SVT <W7SVT@icloud.com>
2021-09-02 09:20:40 -0700

    Initial Copy from 32 bit

---
+++ README.md
@@ -0,0 +1,29 @@
+# baconTelegraph
+ba·con  (bā′kən)
+n.<br>
+1. The salted and smoked meat from the back and sides of a pig. <br> 
+~~2.~~ 1. Proof that God exists and he loves us.  
+
+tel·e·graph  (tĕl′ĭ-grăf′) (from Ancient Greek: τῆλε, têle, "at a distance")
+n.<br>
+1. To send or convey a message to (a recipient) by telegraph.
+1. To give advance notice of.
+   1. To make known (a feeling or an attitude, for example) by nonverbal means: telegraphed her derision with a smirk.
+   1. To make known (an intended action, for example) in advance or unintentionally:
+
+### Assumptions
+Fresh install of RaspberryPi OS
+Love of Ham Radio
+
+### Install
+```
+cd ~
+git clone https://github.com/W7SVT/baconTelegraph.git
+cd ~/baconTelegraph
+/bin/bash install.sh
+```
+
+#### Note: Once you have installed everything you would like a reboot is nessasary
+### CLI Enable I2C port:
+
+sudo raspi-config nonint do_i2c 0
---
+++ install.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#########################################################
+# Created by W7SVT May 2021  ############################
+# Updated by W7SVT June 2021 ############################
+#########################################################
+#########################################################
+#  __      ___________  _____________   _______________ #
+# /  \    /  \______  \/   _____/\   \ /   /\__    ___/ #
+# \   \/\/   /   /    /\_____  \  \   Y   /   |    |    #
+#  \        /   /    / /        \  \     /    |    |    #
+#   \__/\  /   /____/ /_______  /   \___/     |____|    #
+#        \/                   \/                        #
+#########################################################
+echo "######################" 
+echo "# Read/Set CALLSIGN  #"
+echo "######################" 
+
+if [ -n "$CALLSIGN" ]; then
+  echo "Your CALLSIGN is set to '$CALLSIGN'"
+  echo "If you whish to change it please run 'sudo mousepad ~/.bashrc' and change it on the last line"
+else
+  read  -r -p "What is your CALLSIGN?:" GRID
+  sudo echo "export CALLSIGN=$CALLSIGN" >> $HOME/.bashrc
+fi
+
+echo "######################" 
+echo "# Read/Set GRID      #"
+echo "######################" 
+sleep 1
+
+if [ -n "$GRID" ]; then
+  echo "Your GRID is set to '$GRID'"
+  echo "If you whish to change it please run 'sudo mousepad ~/.bashrc' and change it on the last line"
+else
+  read  -r -p "What is your GRID?:" GRID
+  sudo echo "export GRID=$GRID" >> $HOME/.bashrc
+fi
+
+sleep 1
+
+echo "######################" 
+echo "# Zenity Install Menu#"
+echo "######################" 
+
+ask=$(zenity --title "baconTelegraph Install" \
+	--text "Proof that Pi is irrational" \
+	--list \
+	--radiolist \
+	--width=400 \
+	--height=400 \
+	--multiple \
+	--column "Install" --column "Tool" \
+    False "Prerequisites only" \
+    False "hamlib" \
+    False "JTDX" \
+    False "WSJT-X" \
+    False "CHIRP" \
+    False "Conky" \
+    False "Conky Small Screen" \
+    False "JS8call" \
+    )
+echo $ask
+
+case $ask in
+   "Prerequisites only") \
+     sudo apt update && sudo apt upgrade -y
+     sudo apt install -y \
+        curl \
+        cmake \
+        pavucontrol \
+        git \
+        python3 \
+        python3-pip \
+        libasound2-dev \
+        extra-xdg-menus 
+    sudo pip3 install pi-ina219
+    sudo sed -i "s/# en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen
+    sudo locale-gen
+    
+#    pcmanfm --set-wallpaper $HOME/baconTelegraph/files/radioRoom2.jpeg 
+;;
+   "hamlib") \
+/bin/sh $HOME/baconTelegraph/hamlib.sh
+;;
+   "JTDX") \
+/bin/sh $HOME/baconTelegraph/jtdx.sh
+;;
+   "WSJT-X") \
+/bin/sh $HOME/baconTelegraph/wsjtx.sh
+;;
+   "CHIRP") \
+/bin/sh $HOME/baconTelegraph/chirp.sh
+;;
+   "Conky") \
+/bin/sh $HOME/baconTelegraph/conky.sh
+;;
+   "Conky") \
+/bin/sh $HOME/baconTelegraph/conkySM.sh
+;;
+   "JS8call") \
+/bin/sh $HOME/baconTelegraph/js8call.sh
+;;
+   *) \
+echo "Sorry, no selection made"
+exit 0   
+   ;;
+esac
+####Once More
+/bin/sh install.sh
